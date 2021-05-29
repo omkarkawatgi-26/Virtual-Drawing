df5a0e2d5960c1e5b8b9c3ace0f56ced4785245b
omkarkawatgi-26 <85009179+omkarkawatgi-26@users.noreply.github.com>
2021-05-29 12:55:59 +0530

    webcam

--- WebCam.py
+++ WebCam.py
@@ -1,9 +1,9 @@
 import cv2
 import numpy as np
 video = cv2.VideoCapture(0)
-video.set(3,360)
-video.set(4,360)
-#video.set(10,)
+video.set(3,640)#Frame width
+video.set(4,480)#Frame Height
+video.set(10,150)#Frame Brightness
 
 
 
